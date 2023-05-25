f123d2eda51b66c6525fe17c6b36025ccf111bd1
Carlos Eduardo N. Santos <carlos_dinorei@hotmail.com>
2023-05-25 19:03:59 -0300

    Casos de teste 24 a 29 e erros 36 a 37 funcionando

--- src/main/java/br/ufscar/dc/compiladores/alguma/lexico/Principal.java
+++ src/main/java/br/ufscar/dc/compiladores/alguma/lexico/Principal.java
@@ -12,34 +12,33 @@ public class Principal {
 
     public static void main(String[] args) {
         try {
-            // args[0] é o primeiro argumento da linha de comando
+            // args[0] é o primeiro argumento da linha de comando que representa o CAMINHO PARA O ARQUIVO DE ENTRADA COM A LINGUAGEM
             CharStream cs = CharStreams.fromFileName(args[0]);
             AlgumaLexer lex = new AlgumaLexer(cs);
-            // Pega o primeiro argumento da linha de comando
+            // Pega o primeiro argumento da linha de comando que representa o CAMINHO PARA O ARQUIVO DE SAIDA DA ANALISE LEXICA
             String arquivoSaida = args[1];
-            try (PrintWriter pw = new PrintWriter(arquivoSaida)) {
-            } catch (FileNotFoundException fnfe) {
-                System.err.println("O arquivo/diretório não existe:" + args[1]);
-            }
 
             Token t = null;
+
             // Cria um objeto para escrever no arquivo
             try (PrintWriter pw = new PrintWriter(arquivoSaida)) {
                 while ((t = lex.nextToken()).getType() != Token.EOF) {
                     String nomeToken = (String) AlgumaLexer.VOCABULARY.getDisplayName(t.getType());
-                    if (nomeToken.equals("ERRO")) {
+
+                    // Decisões utilizadas para representar de maneira diferente no arquivo de acordo com o token reconhecido 
+                    if (nomeToken.equals("ERRO")) { // Grava na saida o erro personalizado para simbolos nao identificados
                         pw.println("Linha " + t.getLine() + ": " + t.getText() + " - simbolo nao identificado");
                         break;
-                    } else if( nomeToken.equals("COMENTARIO_NAO_FECHADO")) { 
+                    } else if( nomeToken.equals("COMENTARIO_NAO_FECHADO")) { //Grava na saida o erro personalizado para comentario nao fechados
                         pw.println("Linha " + t.getLine() + ": comentario nao fechado");
                         break;
-                    } else if (nomeToken.equals("CADEIA_NAO_FECHADA")) {
+                    } else if (nomeToken.equals("CADEIA_NAO_FECHADA")) { //Grava na saida o erro personalizado para cadeia nao fechadas
                         pw.println("Linha " + t.getLine() + ": cadeia literal nao fechada");
                         break;
                     } else if (nomeToken.equals("IDENT") | nomeToken.equals("CADEIA") |
-                            nomeToken.equals("NUM_INT") | nomeToken.equals("NUM_REAL")) {
+                            nomeToken.equals("NUM_INT") | nomeToken.equals("NUM_REAL")) { // Caso reconheça algum deses tokens, grava o nome do Token e o valor dele.
                         pw.println("<'" + t.getText() + "'," + nomeToken + ">");
-                    } else {
+                    } else { // Todas as outra opções da analise lexica gravam o valor do token reconhecido e novamente seu valor como nos casos de teste
                         pw.println("<'" + t.getText() + "','" + t.getText() + "'>");
                     }
                 }
