9f3e87a595c6ff04840d81de4653a596bc35a0dc
mcv <100847921+mvtehutd@users.noreply.github.com>
2023-05-26 17:52:09 -0300

    Atualização para Entrega do Trabalho

--- src/main/antlr4/br/ufscar/dc/compiladores/alguma/lexico/AlgumaLexer.g4
+++ src/main/antlr4/br/ufscar/dc/compiladores/alguma/lexico/AlgumaLexer.g4
@@ -1,15 +1,20 @@
 lexer grammar AlgumaLexer;
 
+// Palavras chave da gramática, representam palavras que possuem um sentindo/executam uma função na gramática.
 PALAVRA_CHAVE: 'algoritmo' | 'declare' | 'literal' | 'inteiro' | 'real' | 'logico' | 'se' | 'entao' | 'senao' | 
     'fim_se' | 'caso' | 'seja' | 'fim_caso' | 'e' | 'nao' | 'ou' | 'leia' | 'escreva' | 'fim_algoritmo' | 'para' | 
-    'faca' | 'fim_para' | 'ate' | 'tipo' | 'enquanto' | 'fim_enquanto' | 'registro' | 'fim_registro';
+    'faca' | 'fim_para' | 'ate' | 'tipo' | 'enquanto' | 'fim_enquanto' | 'registro' | 'fim_registro' | 'fim_procedimento' | 'var' |
+    'procedimento' | 'funcao' | 'fim_funcao' | 'retorne' | 'constante' | 'falso' | 'verdadeiro';
 
+// Identificadores para números inteiros e reais
 NUM_INT: ('0'..'9')+;
 
 NUM_REAL: ('0'..'9')+ ('.' ('0'..'9')+)?;
 
+// Identifica uma cadeia de caracteres dentro de " ".
 CADEIA: '"' ~('\n'|'\r'|'"')* '"';
 
+// Operadores da gramática que vão desde aritméticas, relacionais, até aqueles que executam alguma ação na gramática.
 DELIM: ':';
 
 INTERVALO: '..';
@@ -24,21 +29,34 @@ ATRIBUICAO: '<-';
 
 PONTEIRO: '^' | '&';
 
+
+// Identifica um comentário que é representado por uma sequência de caracteres dentro de { } e ignora ele
 COMENTARIO: '{' ~('\n'|'\r'|'{'|'}')* '}' {skip();};
 
+// Ignora espaços em branco
 WS: ( ' ' | '\t' | '\r' | '\n') {skip();};
 
+// Identificadores para os [] e () e ,
 ABREPAR: '(';
 
 FECHAPAR: ')';
 
+ABRECHAVE: '[';
+
+FECHACHAVE: ']';
+
 VIRGULA: ',';
 
+// Reconhece os identificadores (variáveis), que são qualquer sequência de caracteres iniciada por uma letra ou _ (underline)
 IDENT: ('a'..'z'|'A'..'Z'|'_') ('a'..'z'|'A'..'Z'|'0'..'9'|'_')*;
 
+// Identificadores que representam os possíveis erros
+// Reconhece uma cadeia que não teve suas "" fechada
 CADEIA_NAO_FECHADA: '"' ~('\n'|'\r'|'"')* '\n';
 
+// Reconhece um comentário que não fechou sua }
 COMENTARIO_NAO_FECHADO: '{' ~('\n'|'\r'|'{'|'}')* '\n' ;
 
+// Qualquer outro símbolo não identificado é reconhecido aqui
 ERRO: .;
 
--- src/main/java/br/ufscar/dc/compiladores/alguma/lexico/Principal.java
+++ src/main/java/br/ufscar/dc/compiladores/alguma/lexico/Principal.java
@@ -12,34 +12,33 @@ public class Principal {
 
     public static void main(String[] args) {
         try {
-            // args[0] é o primeiro argumento da linha de comando
+            // args[0] é o primeiro argumento da linha de comando que representa o CAMINHO PARA O ARQUIVO DE ENTRADA COM A LINGUAGEM
             CharStream cs = CharStreams.fromFileName(args[0]);
             AlgumaLexer lex = new AlgumaLexer(cs);
-            // Pega o primeiro argumento da linha de comando
+            // Pega o segundo argumento da linha de comando que representa o CAMINHO PARA O ARQUIVO DE SAIDA DA ANALISE LEXICA
             String arquivoSaida = args[1];
-            try (PrintWriter pw = new PrintWriter(arquivoSaida)) {
-            } catch (FileNotFoundException fnfe) {
-                System.err.println("O arquivo/diretório não existe:" + args[1]);
-            }
 
             Token t = null;
+
             // Cria um objeto para escrever no arquivo
             try (PrintWriter pw = new PrintWriter(arquivoSaida)) {
                 while ((t = lex.nextToken()).getType() != Token.EOF) {
                     String nomeToken = (String) AlgumaLexer.VOCABULARY.getDisplayName(t.getType());
-                    if (nomeToken.equals("ERRO")) {
+
+                    // Decisões utilizadas para representar de maneira diferente no arquivo de acordo com o token reconhecido 
+                    if (nomeToken.equals("ERRO")) { // Grava na saída o erro personalizado para símbolos não identificados
                         pw.println("Linha " + t.getLine() + ": " + t.getText() + " - simbolo nao identificado");
                         break;
-                    } else if( nomeToken.equals("COMENTARIO_NAO_FECHADO")) { 
+                    } else if( nomeToken.equals("COMENTARIO_NAO_FECHADO")) { // Grava na saída o erro personalizado para comentário não fechados
                         pw.println("Linha " + t.getLine() + ": comentario nao fechado");
                         break;
-                    } else if (nomeToken.equals("CADEIA_NAO_FECHADA")) {
+                    } else if (nomeToken.equals("CADEIA_NAO_FECHADA")) { // Grava na saída o erro personalizado para cadeia não fechadas
                         pw.println("Linha " + t.getLine() + ": cadeia literal nao fechada");
                         break;
                     } else if (nomeToken.equals("IDENT") | nomeToken.equals("CADEIA") |
-                            nomeToken.equals("NUM_INT") | nomeToken.equals("NUM_REAL")) {
+                            nomeToken.equals("NUM_INT") | nomeToken.equals("NUM_REAL")) { // Caso reconheça algum deses tokens, grava o nome do Token e o valor dele.
                         pw.println("<'" + t.getText() + "'," + nomeToken + ">");
-                    } else {
+                    } else { // Todas as outra opções da análise léxica gravam o valor do token reconhecido e novamente seu valor como nos casos de teste
                         pw.println("<'" + t.getText() + "','" + t.getText() + "'>");
                     }
                 }
